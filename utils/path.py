6147c698febac667f70524759f608af823316277
Nikolay <nokolay.zh@gmail.com>
2022-11-16 14:30:39 +0300

    add utils files

---
+++ utils/path.py
@@ -0,0 +1,12 @@
+TRAJ_DIR = "trajectory_data/"
+RESULT_DIR = "result_data/"
+RAW_DIR = "raw_data/"
+IMAGES_DIR = "docs/images/"
+
+GROUND_TRAJECTORY_FILENAME = "ground_traj.npy"
+EXITED_TRAJECTORY_FILENAME = "exited_traj.npy"
+
+GROUND_DISTANCE_MATRIX = "ground_sspd_matrix.npy"
+EXITED_DISTANCE_MATRIX = "exited_sspd_matrix.npy"
+
+NUMBER_OF_TRAJS = 15_000
